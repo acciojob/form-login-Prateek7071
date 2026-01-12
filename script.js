67c6a711a06ea50f1ceeed6fee9f88230748f1c8
Prateek <106758759+Prateek7071@users.noreply.github.com>
2026-01-12 18:10:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
+const Fname = document.getElementByName('fname')
+const Lname = document.getElementByName('lname')
 function getFormvalue() {
     //Write your code here
-
+	alert(`${Fname} ${Lname}`)
 }
