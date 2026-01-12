ead483ce94e30d875d8c7fdf63b46679c637c876
Prateek <106758759+Prateek7071@users.noreply.github.com>
2026-01-12 19:06:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function getFormvalue() {
     //Write your code here
 	event.preventDefault()
 
-	const form=getElementbyId('form1')
+	const form=document.getElementById('form1')
 	const fname = form.elements['fname']
 	const lname = form.elements['lname']
 
